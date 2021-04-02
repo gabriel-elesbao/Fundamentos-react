228da8120052c119258c8bf7cea5c3b3af07022f
Caio Furtado <caiohbfurtado@gmail.com>
2021-04-02 11:04:10 -0300

    ajustes projeto

--- src/index.js
+++ src/index.js
@@ -1,43 +1,5 @@
-import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Card from './components/layout/Card'
+import { App } from './App';
 
-// import Primeiro from './components/Pirmeiro'
-// import WithParameters from './components/WithParameters';
-// import WithChildren from './components/WithChildren';
-
-ReactDOM.render(    // esse metodo vai receber o elemento a qual eu quero redenrizar 
-    <div>
-
-        {/* <Primeiro>  </Primeiro>
-
-        <WithParameters 
-        title="This is the title"
-        subtitle="This is the subtitle"> </WithParameters>
-
-        <WithParameters 
-        title="Opa"
-        subtitle="Epa"> </WithParameters> */}
-        {/* <WithChildren> 
-            <ul>
-              <h1>teste</h1>
-                <li>Ana</li>
-                <li>Calros</li>
-                <li>Bia</li>
-                <li>Daniel</li>
-            </ul>
-        </WithChildren> */}
-        <div>
-            <Card title = "Exercicio X">
-                 Conteudo
-            </Card>  
-            
-        </div>
-
-    </div>,
-    
-
-    document.getElementById('root')
-);  
- 
+ReactDOM.render(<App />, document.getElementById('root'));
